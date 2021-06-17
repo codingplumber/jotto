ead381c9171533125eed8d0d7b1b0f0219506ccd
Jason Stickel <jason.stickel@live.com>
2021-06-17 15:33:36 -0500

    Tests and code for non-zero guessed words

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -26,6 +26,7 @@ describe('if there are no words guessed', () => {
     beforeEach(() => {
         wrapper = setUp({ guessedWords: [] });
     });
+
     test('renders without error', () => {
         const component = findByTestAttr(wrapper, 'component-guessed-words');
         expect(component.length).toBe(1);
@@ -38,5 +39,28 @@ describe('if there are no words guessed', () => {
 });
 
 describe('if there are words guessed', () => {
+    let wrapper;
+    const guessedWords = [
+        { guessWord: 'train', letterMatchCount: 3 },
+        { guessWord: 'agile', letterMatchCount: 1 },
+        { guessWord: 'party', letterMatchCount: 5 },
+    ];
+    beforeEach(() => {
+        wrapper = setUp({ guessedWords });
+    });
 
+    test('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+
+    test('renders "guessed words" section', () => {
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+        expect(guessedWordsNode.length).toBe(1);
+    });
+
+    test('correct number of guessed words', () => {
+        const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+        expect(guessedWordsNodes.length).toBe(guessedWords.length);
+    });
 });
